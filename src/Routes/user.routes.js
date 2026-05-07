fba0c0fc29f4218d4b81b4c01e07e3c88c268a1d
HarendraRawat17 <yt2022a3@gmail.com>
2026-05-07 14:09:29 +0530

    fixed some bugs of registerController & ApiError

--- src/Routes/user.routes.js
+++ src/Routes/user.routes.js
@@ -13,7 +13,7 @@ router.route('/register').post(
       maxCount: 1
     },
     {
-      name: "converImage",
+      name: "coverImage",
       maxCount: 1
     }
   ]),
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,7 +7,7 @@ import {ApiResponse} from "../Utils/ApiResponse.js"
 
 const registerUser = asyncHandler(async (req, res) => {
   const {fullName, email, username, password} = req.body
-  console.log("email", email);
+  // console.log("email", email);
 
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -15,7 +15,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required")
   }
 
-  const existingUser = User.findOne({
+  const existingUser = await User.findOne({
     $or: [{ username }, { email }]
    })
 
@@ -24,7 +24,12 @@ const registerUser = asyncHandler(async (req, res) => {
    }
 
    const avatarLocalPath = req.files?.avatar[0]?.path; // files is given by multer middleware
-   const coverImageLocalPath = req.files?.coverImage[0]?.path;
+   // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+   let coverImageLocalPath;
+   if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+    coverImageLocalPath = req.files.coverImage[0].path
+   }
 
    if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required"); 
@@ -37,7 +42,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Avatar file is required"); 
    }
 
-   User.create({
+   const user = await User.create({
     fullName,
     avatar: avatar.url,
     coverImage: coverImage?.url || "",
